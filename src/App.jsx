05cc2ae72379d5cac9e505699af4b3e8251b3153
Ahmad Hassan <ahmhass313786@gmail.com>
2025-05-26 19:00:54 +0500

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,15 @@ function App() {
   return (
     <>
       {!isLoaded && <LoadingScreen onComplete={()=> setIsLoaded(true)} />}
-        <div className={`min-h-screen transition-opacity duration-700 ${isLoaded ? "opacity-100":"opacity-0"} bg-black text-gray-100 `} >
+        <div className={`min-h-screen my-16 w-[575px] sm:w-full sm:my-0  transition-opacity duration-700 ${isLoaded ? "opacity-100":"opacity-0"} bg-black text-gray-100 `} >
           <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
           <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          <div className="" >
           <Home/>
           <About/>
           <Projects/>
           <Contact/>
+          </div>
         </div>
     </>
   )
