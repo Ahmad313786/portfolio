118b8ec78e4112020acdf4746c667e9ccb85b422
Ahmad Hassan <ahmhass313786@gmail.com>
2025-05-29 23:38:18 +0500

    Update project with new changes

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -21,14 +21,14 @@ const Projects = () => {
           </div>
           <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59, 130, 246,0.2) transition-all' >
             <h3 className='text-xl font-bold mb-2' >E-commerce Website</h3>
-            <p className='text-gray-300 mb-4' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus nobis minus odit voluptatum architecto reprehenderit.</p>
+            <p className='text-gray-300 mb-4' >Ecommerce website from where you can add products to your cart and then from checkout you can proceed to payment.You can also select the quantity of product.</p>
             <div className='flex gap-1' >
-              {["React", "Node.js", "MongoDB"].map((tech, key) => (
+              {["React", "Nextjs", "ShadCN", "Stripe"].map((tech, key) => (
                 <span key={key} className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow[0_2px_8px_rgba(59, 130, 246,0.1) transition-all' >{tech}</span>
               ))}
             </div>
             <div className='flex justify-between items-center' >
-              <a href="#" className='text-blue-400 hover:text-blue-300 transition-colors my-4'>View Project ➡️</a>
+              <a href="https://ecom-lime-three.vercel.app/" className='text-blue-400 hover:text-blue-300 transition-colors my-4'>View Project ➡️</a>
             </div>
           </div>
           <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59, 130, 246,0.2) transition-all' >
