e1a6566b8d48733af5534bec22ec3dbd48e8ded5
Ahmad Hassan <ahmhass313786@gmail.com>
2025-07-16 11:36:31 +0500

    Your commit message

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -8,6 +8,18 @@ const Projects = () => {
         <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'  >Featured Projects</h2>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-6' >
           <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59, 130, 246,0.2) transition-all' >
+            <h3 className='text-xl font-bold mb-2' >MyShop - Ecommerce App</h3>
+            <p className='text-gray-300 mb-4' >A fully responsive frontend e-commerce website built with React. Users can browse, search, and filter products, add items to the cart, and place orders — all through a smooth and intuitive interface.</p>
+            <div className='flex gap-1' >
+              {["ReactJS", "TailwindCSS"].map((tech, key) => (
+                <span key={key} className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow[0_2px_8px_rgba(59, 130, 246,0.1) transition-all' >{tech}</span>
+              ))}
+            </div>
+            <div className='flex justify-between items-center' >
+              <a href="https://ahmad313786.github.io/myshop"  className='text-blue-400 hover:text-blue-300 transition-colors my-4' target='_blank' >View Project ➡️</a>
+            </div>
+          </div>
+          <div className='p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow[0_2px_8px_rgba(59, 130, 246,0.2) transition-all' >
             <h3 className='text-xl font-bold mb-2' >Todo - List App</h3>
             <p className='text-gray-300 mb-4' >Todo List app which stores todo in local storage.Todos can be edit and deleted.You can also filter todos to hide completed todos</p>
             <div className='flex gap-1' >
